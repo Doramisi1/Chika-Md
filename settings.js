480e37c38ce65fa68ee518e8fa1ccb252b20494c
Doramisi1 <105469411+Doramisi1@users.noreply.github.com>
2022-06-26 12:58:01 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -22,14 +22,15 @@ global.APIKeys = {
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6281220670449','6283802603905' , '62083195893959' , '62887435047326' , '6281368176386' , '6281575886399']
+global.packname = 'Sky•BOT'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6281220670449','6283802603905' , '62083195893959' , '62887435047326' , '6281368176386' , '6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6281220670449','6283802603905' , '62083195893959' , '62887435047326' , '6281368176386' , '6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'SKY•BOT' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'SKY•BOT-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'EGA•GANS' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
@@ -37,12 +38,12 @@ global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
 global.ig = 'https://instagram.com/riychdwayne'
 global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
 global.myweb = 'https://s.id/chikabot'
-global.email = 'riychdwaynee@gmail.com'
+global.email = 'EGABERTOANANTA@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© SKY-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'SKY' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
@@ -52,7 +53,7 @@ global.sessionName = 'session'
 //terserah mau ubah atau nggak, note : tanda ' gausah di hapus!
 global.limitawal = {
 premium: "Infinity",
-free: 15,
+free: 99,
 }
 
 //—————「 Set Image 」—————//
